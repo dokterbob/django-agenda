3b06212bf6e63d3b57ffd9f3bf676219541c505a
Mathijs de Bruin <drbob@devel.visualspace.nl>
2008-10-11 12:01:41 +0200

    Timezone bug in settings.

--- settings.py
+++ settings.py
@@ -29,7 +29,7 @@ else:
 # although not all choices may be available on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'Amsterdam/Europe'
+TIME_ZONE = 'Europe/Amsterdam'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
