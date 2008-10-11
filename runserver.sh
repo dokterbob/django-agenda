c0a9407be2c58ebb2a0f7faab96099b690b7e964
Mathijs de Bruin <mathijs@visualspace.nl>
2008-10-11 13:28:17 +0200

    Runserver cleaned up, port number now in separate file.

--- runserver.sh
+++ runserver.sh
@@ -5,11 +5,21 @@ if [ -f /usr/local/bin/use-django ]; then
     use-django r9067
 fi
 
+cd `dirname $BASH_SOURCE`
+
 PWD=`pwd`
 BASEPATH=`basename $PWD`
+
+if [ ! -f portnumber ]; then
+    echo 'No port number set!'
+    echo 'Please create a file named "portnumber" in the current directory with'
+    echo 'a locally unique portnumber in it, like "1234".'
+    exit -1
+fi
+
 if [[ $1 != "" ]]; then
     python manage.py $1
 else
-    screen -S $BASEPATH python manage.py runserver `hostname`:7483
+    screen -S $BASEPATH python manage.py runserver `hostname`:`cat portnumber`
 fi
 
