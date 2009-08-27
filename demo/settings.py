cab5eda34b6cbd46024101357ad9daef8d770956
kramer <kramer@mail.bucurestiulmeu.ro>
2009-08-28 07:08:00 +0800

    Changed demo default language.
    Signed-off-by: Mathijs de Bruin <drbob@dokterbob.net>

--- demo/settings.py
+++ demo/settings.py
@@ -37,7 +37,7 @@ TIME_ZONE = 'Europe/Amsterdam'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'nl-nl'
+LANGUAGE_CODE = 'en'
 
 SITE_ID = 1
 
